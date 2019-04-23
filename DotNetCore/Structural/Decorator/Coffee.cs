dc232e80e19c46434019ce0ae9def33dab29d600
Manu <emmanuel.conrardy@arolla.fr>
2019-04-23 16:34:16 +0200

    Add Decorator

---
+++ DotNetCore/Structural/Decorator/Coffee.cs
@@ -0,0 +1,128 @@
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Decorator
+{
+    #region without_design
+    [Fact]
+    public void Adapter_Espresso_WithoutDesign()
+    {
+        var coffee = new Espresso();
+
+        var result = coffee.GetCost();
+
+        Assert.Equals(2.99, result);
+    }
+
+    [Fact]
+    public void Adapter_EspressoWithMilk_WithoutDesign()
+    {
+        var coffee = new EspressoWithMilk();
+
+        var result = coffee.GetCost();
+
+        Assert.Equals(3.99, result);
+    }
+    public class Filtered
+    {
+        public string GetDescription()
+        {
+            return "Filtered with care";
+        }
+    
+        public double GetCost()
+        {
+            return 1.99;
+        }
+    }
+    
+    public class Espresso
+    {
+        public string GetDescription()
+        {
+            return "Espresso made with care";
+        }
+    
+        public double GetCost()
+        {
+            return 2.99;
+        }
+    }
+
+    public class EspressoWithMilk
+    {
+        public string GetDescription()
+        {
+            return "Espresso made with care, with milk";
+        }
+    
+        public double GetCost()
+        {
+            return 3.99;
+        }
+    }
+    #endregion
+
+    #region with_design
+    
+    [Fact]
+    public void Adapter_Expresoo_Chocolate_WitDesign()
+    {
+        var coffee = new ChocolateDecorator(new Espresso());
+
+        var result = coffee.GetCost();
+
+        AssemblyLoadEventArgs.Equals(2.99, result);
+    }
+    
+    public interface ICoffee
+    {
+        string GetDescription();
+        double GetCost();
+    }
+
+    public abstract class CondimentDecorator : ICoffee
+    {
+        ICoffee _coffee;
+    
+        protected string _name = "undefined condiment";
+        protected double _price = 0.0;
+    
+        public CondimentDecorator(ICoffee coffee)
+        {
+            _coffee = coffee;
+        }
+    
+        public string GetDescription()
+        {
+            return string.Format("{0}, {1}", _coffee.GetDescription(), _name);
+        }
+    
+        public double GetCost()
+        {
+            return _coffee.GetCost() + _price;
+        }
+    }
+    
+    public class MilkDecorator : CondimentDecorator
+    {
+        public MilkDecorator(ICoffee coffee)
+            :base(coffee)
+        {
+            _name = "Milk";
+            _price = 1;
+        }
+    }
+ 
+    public class ChocolateDecorator : CondimentDecorator
+    {
+        public ChocolateDecorator(ICoffee coffee)
+            :base(coffee)
+        {
+            _name = "Chocolate";
+            _price = 2;
+        }
+    }
+    #endregion
+}
---
+++ DotNetCore/Structural/Decorator/readme.md
@@ -0,0 +1,10 @@
+# Decorator
+le Design Pattern Decorator permet d’ajouter dynamiquement des responsabilit&eacute;s à un objet existant et que cette solution apporte plus de souplesse que l’h&eacute;ritage.
+
+## Images / analogie
+
+Ce pattern est une enveloppe, on va englober l'objet source pour ajouter ou modifier une fonctionalitée.
+
+## Liens
+
+https://refactoring.guru/design-patterns/decorator
